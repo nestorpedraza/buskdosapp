6fbba668ed49630b4a0c919ee24412922b60690f
nestorpedrazaa <nestorpedrazaa@gmail.com>
2025-12-13 10:16:48 -0500

    se areglan rutas del home

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,7 +23,7 @@ export default function AuthForm({ type }: AuthFormProps) {
             setValidationError('');
             // Lógica de envío...
             if (type === 'login') {
-                router.push('/HomeScreen');
+                router.push('/homescreen');
             }
         }
     };
