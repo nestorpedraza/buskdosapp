8b428f353dfd7f8c3bb27380b7f6b1f542fab5ff
nestorpedrazaa <nestorpedrazaa@gmail.com>
2025-12-16 17:22:31 -0500

    se agrega funcionalidad de cierre de teclado en el login cuando se presiona afuera

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
 import AuthBackground from '../components/AuthBackground';
 import AuthForm from '../components/AuthForm';
 
 export default function Login() {
     return (
-        <AuthBackground>
-            <AuthForm type="login" />
-        </AuthBackground>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={{ flex: 1 }}>
+                <AuthBackground>
+                    <AuthForm type="login" />
+                </AuthBackground>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
